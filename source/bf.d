430eecebc386f69cb18c28eba31edbff51bda71b
Cameron Ross <elpenguino@gmail.com>
2016-01-27 18:03:37 -0400

    separate compiler logic into a different module, enable stdin/stdout redirection, minor optimizations

---
+++ source/bf.d
@@ -0,0 +1,90 @@
+import std.range : isInputRange, isOutputRange;
+///A brainfuck program
+struct BFProgram(string program, size_t memSize = 0x1000) {
+	///Just a big ol' block of memory
+	dchar[memSize] memory = void;
+	///Program data pointer
+	size_t ptr = 0;
+
+	///Compile and execute program
+	void execute() {
+		import std.stdio : stdin, stdout;
+		struct StdInput {
+			char lastRead;
+			bool empty() { return stdin.isOpen; }
+			char front() { return lastRead; }
+			void popFront() { if (stdin.rawRead([lastRead]).length == 0) stdin.close(); }
+		}
+		auto inp = StdInput();
+		auto outp = stdout.lockingTextWriter;
+		execute(inp, outp);
+	}
+	void execute(Input, Output)(ref Input stdin, ref Output stdout) if (isInputRange!Input && isOutputRange!(Output, char)) {
+		import std.range : put, front, popFront, empty;
+		import std.algorithm : copy;
+		memory[] = '\0';
+		mixin(transpile(program));
+	}
+}
+///Transpile bf -> D
+string transpile(string program) pure @safe {
+	import std.string : join, format;
+	import std.algorithm : group;
+	import std.range : repeat;
+	import std.array : array;
+	string[] prog;
+	foreach (instruction, count; program.group) {
+		switch(instruction) {
+			case '+':
+				prog ~= format("memory[ptr] += %d;", count);
+				break;
+			case '-':
+				prog ~= format("memory[ptr] -= %d;", count);
+				break;
+			case '>':
+				prog ~= format("ptr += %d;", count);
+				break;
+			case '<':
+				prog ~= format("ptr -= %d;", count);
+				break;
+			case '.':
+				prog ~= "copy([memory[ptr]], stdout);".repeat(count).array;
+				break;
+			case ',':
+				prog ~= `if (!stdin.empty) { memory[ptr] = stdin.front; stdin.popFront(); }`.repeat(count).array;
+				break;
+			case '[':
+				prog ~= `while(memory[ptr] != '\0') {`.repeat(count).array;
+				break;
+			case ']':
+				prog ~= `}`.repeat(count).array;
+				break;
+			default: break;
+		}
+	}
+	return prog.join("\n");
+}
+unittest {
+	import std.array : appender;
+	{
+		auto program = BFProgram!(import("helloworld.b"), 7)();
+		auto outbuf = appender!(char[])();
+		char[] inbuf;
+		program.execute(inbuf, outbuf);
+		assert(outbuf.data == "Hello World!\n");
+	}
+	{
+		auto program = BFProgram!(import("bench.b"), 8)();
+		auto outbuf = appender!(char[])();
+		char[] inbuf;
+		program.execute(inbuf, outbuf);
+		assert(outbuf.data == "ZYXWVUTSRQPONMLKJIHGFEDCBA\n");
+	}
+	{
+		auto program = BFProgram!(import("rot13.b"), 8)();
+		auto outbuf = appender!(char[])();
+		string inbuf = "HELLO";
+		program.execute(inbuf, outbuf);
+		assert(outbuf.data == "URYYB");
+	}
+}
